d70aab6035b908b0dec1e819f3b37ca2e4fa3491
Ali Ariff <ali.ariff12@gmail.com>
2017-05-09 22:29:13 +0200

    add deployment flow

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,31 @@
 # config valid only for current version of Capistrano
-lock "3.8.1"
+lock '3.8.1'
 
-set :application, "node_app"
-set :user, "webtech"
-set :repo_url, "https://github.com/aliariff/webtech-deployment.git"
+set :application, 'node_app'
+set :user, 'webtech'
+set :repo_url, 'https://github.com/aliariff/webtech-deployment.git'
 set :deploy_to, "/home/#{fetch(:user)}"
+after 'deploy:finishing', 'deploy:restart_docker'
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart_docker do
+    on roles(:web_app) do |host|
+      execute "cd #{deploy_to}/current && docker build --rm=true -t #{fetch(:application)} ."
+      execute "docker stop #{fetch(:application)}; echo 0"
+      execute "docker rm -fv #{fetch(:application)}; echo 0"
+      execute "docker run -d -p 80:3000 --name #{fetch(:application)} #{fetch(:application)}"
+    end
+  end
+
+  desc 'Stop application'
+  task :stop_docker do
+    on roles(:web_app) do |host|
+      execute "docker stop #{fetch(:application)}; echo 0"
+      execute "docker rm -fv #{fetch(:application)}; echo 0"
+    end
+  end
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
