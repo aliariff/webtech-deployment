a6c12f214d69797f08cd4ce7ccbb1d56b6ad4382
Ali Ariff <ali.ariff12@gmail.com>
2017-05-09 20:56:18 +0200

    show hostname

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
+var os = require('os')
 
 app.get('/', function (req, res) {
-  res.send('Hello Advanced Web Technologies Class!')
+  res.send('Hello Advanced Web Technologies Class! - Hostname: ' + os.hostname())
 })
 
 app.listen(3000, function () {
